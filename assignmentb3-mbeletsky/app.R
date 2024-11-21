8d1baa3ea1bad2d9c5ccbd0a03dc25923ca5993a
mbeletsky <beletskymasha12@gmail.com>
2024-11-21 08:19:07 -0800

    committing work to branch

---
+++ assignmentb3-mbeletsky/app.R
@@ -0,0 +1,23 @@
+library(shiny)
+library(tidyverse)
+#Photo attribution: kamchatka on FreePik
+grizzly <- read.csv("grizzly.csv", stringsAsFactors = FALSE)
+ui <- fluidPage(
+  titlePanel("Grizzly Population - British Columbia, Canada (2015 and 2018)"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("year", "Year:",
+                  c("2015" = "EST_POP_2015",
+                    "2018" = "EST_POP_2018")),
+    ),
+    mainPanel(
+      tableOutput("grzly_year")
+    )
+  )
+)
+server <- function(input, output) {
+  output$grzly_year <- renderTable({
+    grizzly[, c("REGION_RESPONSIBLE_NAME", input$year), drop = FALSE]
+  }, rownames = TRUE)
+}
+shinyApp(ui = ui, server = server)
